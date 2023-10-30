f831101f9c9eed5fdded800ee32de07083787a57
Rahulkotha1 <rahul.kotha27@gmail.com>
2023-10-30 22:20:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Colorpicker from './components/Colorpicker';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Colorpicker />
+    </div>
+  );
+}
+
+export default App;
